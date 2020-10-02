4de406713c27a2d294d46233c38eef4295702b7c
Tahirih <tahysluana@gmail.com>
2020-10-01 22:16:49 -0500

    Refactor style in home

---
+++ src/app/home/components/home-info/home-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-info',
+  templateUrl: './home-info.component.html',
+  styleUrls: ['./home-info.component.scss']
+})
+export class HomeInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { CodeTidbitsComponent } from './components/code-tidbits/code-tidbits.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeInfoComponent } from './components/home-info/home-info.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    CodeTidbitsComponent
+    CodeTidbitsComponent,
+    NavbarComponent,
+    HomeInfoComponent
   ],
   exports: [
     HomeComponent
